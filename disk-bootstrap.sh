ebceee18f98c040cb540410107151f4558467924
Peter <alulord@gmail.com>
2021-01-30 20:55:30 +0100

    try binding efivars and others for systemd-nspawn to correctly install refind

--- disk-bootstrap.sh
+++ disk-bootstrap.sh
@@ -124,13 +124,9 @@ function install_system() {
     --register=no \
     --settings=false \
     --bind-ro=/install:/install \
-    --bind=/proc:/proc \
+    --bind-ro=/proc:/proc \
     --bind-ro=/sys:/sys \
-    --bind=/sys/firmware/efi/efivars:/sys/firmware/efi/efivars \
-    --bind=/dev:/dev \
-    --bind=/dev/pts:/dev/pts \
-    --bind=/dev/shm:/dev/shm \
-    --bind=/run:/run \
+    --bind-ro=/sys/firmware/efi/efivars:/sys/firmware/efi/efivars \
     --directory=/mnt \
       ansible-playbook /install/playbook.yaml -M /install/library/ansible-aur/library -i /install/localhost -l "$host" --extra-vars "user_password=$(cat $root_pass_file)"
 }
